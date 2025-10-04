6a8fc1deef799e9651d5fb072700c44cf880fb74
Raul Melnek <miguel.rauldesouzasantos@gmail.com>
2025-10-04 20:28:40 -0300

    Add files via upload

--- src/TesteProjetoCadastro.java
+++ src/TesteProjetoCadastro.java
@@ -1,10 +1,15 @@
+package Cadastro;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
+//Para salvar uma pessoa por arquivo
 public class TesteProjetoCadastro {
     public static void main(String[] args) throws Exception {
             Scanner sc = new Scanner(System.in);
             System.out.println("Cadastro de Clientes");
             
+            //Dados pessoais
                     System.out.println("Nome completo: ");
                 String nome =sc.nextLine();
                     System.out.println("Email: ");
@@ -14,17 +19,29 @@ public class TesteProjetoCadastro {
                     System.out.println("Endereço e número: ");
                 String endereco =sc.nextLine();
                 
+            //Confirma
             System.out.println("Confirme os dados abaixo:");
-            System.out.println("Nome: " + nome);
-            System.out.println("Email: " + email);
-            System.out.println("Telefone: " + telefone);
-            System.out.println("Endereço: " + endereco);
+            System.out.println("Nome: " + nome + "\n" + "Email: " + email + "\n" 
+            + "Telefone: " + telefone + "\n" + "Endereço: " + endereco);
+
 
             System.out.println("Deseja confirmar os dados? (S/N)");
             String confirmar = sc.nextLine();
             if (confirmar.equalsIgnoreCase("S")) {
                 System.out.println("Cadastro realizado com sucesso!");
             }
+            else {
+                System.out.println("Cadastro cancelado.");
+            }
+
+            try (FileWriter writer = new FileWriter("usuários_salvo.txt")) {
+            writer.write("Nome: " + nome + "\nEmail: " + email + "\nTelefone: " +
+            telefone + "\nEndereço: " + endereco +"\n");
+        } catch (IOException e) {
+            System.err.println("Erro ao salvar o arquivo: " + e.getMessage());
+        } finally {
             sc.close();
+    }
+
     }
 }
